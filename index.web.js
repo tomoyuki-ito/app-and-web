28164cac5fc70422e060968fc33107907d3abfa2
Tomoyuki Ito <ito@nagisa-inc.jp>
2018-03-27 12:56:02 +0900

    feat: add webpack config

---
+++ index.web.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import App from './src/App'
+
+AppRegistry.registerComponent('App', () => App);
+
+AppRegistry.runApplication('App', { rootTag: document.getElementById('root') });
+
+// デバッグは以下のコマンドをターミナルで実行
+// ./node_modules/.bin/webpack-dev-server -d --config ./web/webpack.config.js --inline --hot --colors
+
+// リリース用バンドル出力は以下のコマンドをターミナルで実行
+// ./node_modules/.bin/webpack -p --config ./web/webpack.config.js
---
+++ web/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const appDirectory = path.resolve(__dirname, '../');
+
+const babelLoaderConfiguration = {
+  test: /\.js$/,
+  include: [
+    path.resolve(appDirectory, 'index.web.js'),
+    path.resolve(appDirectory, 'src')
+  ],
+  use: {
+    loader: 'babel-loader',
+    options: {
+      cacheDirectory: true,
+      plugins: ['react-native-web'],
+      presets: ['react-native']
+    }
+  }
+};
+
+module.exports = {
+  devServer: {
+    contentBase: path.resolve(appDirectory, 'web/public')
+  },
+  entry: path.resolve(appDirectory, 'index.web.js'),
+  output: {
+    filename: 'bundle.web.js',
+    path: path.resolve(appDirectory, 'web/public')
+  },
+  module: {
+    rules: [
+      babelLoaderConfiguration
+    ]
+  }
+}
